c9507c0dc1b82022b6f5f1163d486a81128b24fb
Danilin1991 <danilopereira261@gmail.com>
2024-02-18 14:14:49 -0300

    cursinho origamid

---
+++ variáveis/script.js
@@ -0,0 +1,13 @@
+console.log("oi");
+var nome = 'andré'
+console.log(nome);
+var idade = 28;
+console.log(nome, idade);
+
+var preco = 25;
+var totalcomprado = 5;
+var totalpreco = totalcomprado * preco;
+
+console.log(totalpreco)
+
+var semdefinir;
