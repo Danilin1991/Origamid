01ff892221a98ef3b3430c596e3f021028beba51
Danilin1991 <danilopereira261@gmail.com>
2024-02-19 20:38:51 -0300

    aprendendo sobre strings

---
+++ tipos de dados/ex_string.js
@@ -0,0 +1,19 @@
+var n1 = "2024";
+// declare uma variavel contendo um numero dentro de uma string
+var idade = 32;
+// declare uma variavel com sua idade
+
+var nome = "Danilo ";
+// declare uma variavel com seu nome
+
+var sobreNome = "Pereira";
+// declare uma variavel com seu sobrenome
+
+var frase = `It' s time`;
+// declare uma variavel com a frase It's time
+
+console.log(nome + sobreNome);
+//some as variaveis com seu nome e sobrenome
+console.log(frase);
+console.log(typeof nome);
+//verifique o tipo da variave que contém  seu nome
---
+++ tipos de dados/template_strings.js
@@ -0,0 +1,7 @@
+var gols = 1000;
+var frase1 = "romario fez " + gols + " gols";
+var frase2 = `romario fez ${gols} gols`;
+
+// usando Template strings ,você deve passar expressões / variaveis dentro de ${}
+console.log(frase1);
+console.log(frase2);
