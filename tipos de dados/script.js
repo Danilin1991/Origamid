c7453c95ee4417d048c4f4db75bd48b1b53ee550
Danilin1991 <danilopereira261@gmail.com>
2024-02-19 20:06:43 -0300

    aprendendo sobre strings

--- tipos de dados/script.js
+++ tipos de dados/script.js
@@ -2,4 +2,5 @@ var idade = 28;
 var time = null;
 console.log(typeof idade);
 
-var nome = andre;
+var nome = "andre";
+console.log(nome);
---
+++ tipos de dados/string.js
@@ -0,0 +1,17 @@
+var nome = "danilo ";
+var sobrenome = "pereira ";
+var nomeCompleto = nome + sobrenome;
+
+console.log(nomeCompleto);
+
+var gols = 1000;
+var frase = "romário fez " + gols + "gols";
+console.log("frase");
+// numero somado com string o resultado sempre será uma string //
+
+var ano = 2024; //'2024' string / 2024 Number
+var mes = 2;
+console.log(ano + mes);
+
+var frase1 = "esse é o melhor jogo";
+console.log(frase1);
