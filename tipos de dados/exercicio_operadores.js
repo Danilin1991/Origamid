0daa8fafb0b51843b6827583279965a19346d1a8
Danilin1991 <danilopereira261@gmail.com>
2024-02-20 19:03:22 -0300

    ..

---
+++ tipos de dados/exercicio_operadores.js
@@ -0,0 +1,18 @@
+// qual o resultado da seguinte expressão?
+var total = 10 + (5 * 2) / 2 + 20;
+
+// Crie duas expressões que retornem NaN
+
+var n1 = "10";
+var n2 = "5";
+
+// Somar a string '200' com o número 50 e retornar 250
+var soma = +"200" + 50;
+// Incremente o número 5 e retorne o seu valor incrementado
+var Incremente = +5;
+// Como dividir o peso por 2?
+var numero = + '80' / 2;
+var unidade = 'kg';
+var peso = numero + unidade; // '80kg'
+
+console.log(++Incremente, soma, peso, total);
---
+++ tipos de dados/unários.js
@@ -0,0 +1,18 @@
+var incremento = 5;
+console.log(incremento++); // 5
+console.log(incremento); // 6
+
+var incremento2 = 5;
+console.log(++incremento2); // 6
+console.log(incremento2); // 6
+
+var frase = "isso é um teste" + frase; // Nan
+-frase; // Nan
+
+var idade = "32";
++idade; // 32 (numero)
+-idade; // -32 (numero)
+console.log(+idade + 8); // 40
+
+var possuiFaculdade = true;
+console.log(+possuiFaculdade); // 1
