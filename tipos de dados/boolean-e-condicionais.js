c535f783dbdf990b4cf24760b8fc0f50af7f26d4
Danilin1991 <danilopereira261@gmail.com>
2024-02-22 18:24:02 -0300

    Create boolean-e-condicionais.js

---
+++ tipos de dados/boolean-e-condicionais.js
@@ -0,0 +1,12 @@
+// condicionais If e Else
+//verificar se se uma expressão é verdadeira com if, caso contrario o else é ativado.
+
+var possuiGraduacao = true;
+
+if (possuiGraduacao) {
+  console.log('Possui Graduacao');
+} else {
+  console.log('Nao possui Graduacao');
+}
+
+// retorna possui graduação, pois o valor da variável possuiGraduacao é true ,o valor dos parenteses será avaliado em false ou true
